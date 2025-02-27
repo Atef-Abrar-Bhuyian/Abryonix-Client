b1f75b61c5b588e7e0d408b942a8d92fe69e7cfe
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-02-28 00:49:33 +0600

    all images page done

--- src/pages/AllGeneratedImage/AllGeneratedImages.jsx
+++ src/pages/AllGeneratedImage/AllGeneratedImages.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
 import Loader from "../../components/Loader/Loader";
+import { Link } from "react-router";
 
 const AllGeneratedImages = () => {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null); 
+  const [error, setError] = useState(null);
   const axiosPublic = useAxiosPublic();
 
   useEffect(() => {
@@ -14,6 +15,7 @@ const AllGeneratedImages = () => {
         const res = await axiosPublic.get("/api/v1/image/all");
         setImages(res.data);
       } catch (error) {
+        console.log(error);
         setError("Error fetching images");
       } finally {
         setLoading(false);
@@ -27,19 +29,25 @@ const AllGeneratedImages = () => {
   if (error) return <div>{error}</div>;
 
   return (
-    <div className="mt-24">
+    <div className="container mx-auto mt-26 ">
       {/* Using the columns utility to create masonry effect */}
-      <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 min-h-screen">
         {images.map((image) => (
           <div
             key={image?._id}
-            className="bg-gray-300 rounded-lg shadow-lg mb-6 break-inside-avoid"
+            className="relative rounded-lg shadow-lg mb-6 group overflow-hidden"
           >
-            <img
-              src={image?.medium_image}
-              alt={"AI Generated Image"}
-              className="w-full h-auto object-cover rounded-lg"
-            />
+            <Link to={`/image/${image?._id}`}>
+              <img
+                src={image?.medium_image}
+                alt="AI Generated Image"
+                className="w-full h-full object-cover rounded-lg cursor-pointer transition duration-300 ease-in-out group-hover:brightness-75"
+              />
+              {/* Text Overlay */}
+              <div className="absolute bottom-0 left-0 w-full p-2 text-white text-sm bg-opacity-50 py-2 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                {image.prompt.slice(1, 80)}...
+              </div>
+            </Link>
           </div>
         ))}
       </div>
